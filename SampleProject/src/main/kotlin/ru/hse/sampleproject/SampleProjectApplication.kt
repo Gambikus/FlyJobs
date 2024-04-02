04fb7c94cad2af745ec0917df37da09faad34e34
chasgbc <aschalenko@edu.hse.ru>
2024-04-02 08:26:44 +0300

    integrate library to sample project

--- SampleProject/src/main/kotlin/ru/hse/sampleproject/SampleProjectApplication.kt
+++ SampleProject/src/main/kotlin/ru/hse/sampleproject/SampleProjectApplication.kt
@@ -1,5 +1,6 @@
 package ru.hse.sampleproject
 
+import config.FlyJobConfigurator
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,18 @@ import org.springframework.boot.runApplication
 class SampleProjectApplication
 
 fun main(args: Array<String>) {
+
+    val config = FlyJobConfigurator("jdbc:postgresql://localhost:5432/lib_db", "user", "password")
+    config.getScheduler().scheduleJob(
+        "*/2 * * * *",
+        Second::print
+    )
     runApplication<SampleProjectApplication>(*args)
+}
+
+
+class Second {
+    public fun print() {
+        println("hello")
+    }
 }
